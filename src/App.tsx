682d2be3b59f8b63724bb7dbb168cee947bee84c
CallumHoward <7144420+CallumHoward@users.noreply.github.com>
2024-08-06 15:53:12 +1000

    Add reactivity

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import {
   type Config,
   ConfigProvider,
   SearchInput,
+  Typography,
   VStack,
 } from "@safetyculture/sc-web-ui/react";
 import { localization } from "./lib/localization";
 import { maggieDark } from "@safetyculture/sc-web-ui/theme";
 import { Library } from "./components/Library";
+import { useState } from "react";
 
 const uiConfig: Config = {
   localization,
@@ -15,13 +17,20 @@ const uiConfig: Config = {
 };
 
 function App() {
+  const [value, setValue] = useState<string>();
+
   return (
     <ConfigProvider config={uiConfig}>
       <main>
         <VStack align="flex-end" spacing="s8">
           <MySideSheet />
           <VStack align="center">
-            <SearchInput size="lg" style={{ maxWidth: "500px" }} />
+            <SearchInput
+              size="lg"
+              style={{ maxWidth: "500px" }}
+              onChange={(e) => setValue(e)}
+            />
+            <Typography variant="bodyMedium">{value}</Typography>
           </VStack>
           <Library />
         </VStack>
